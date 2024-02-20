05ce0bf41a490cc6f889f44d8304760533338fbe
Rishabh Garcha <rish889@gmail.com>
2024-02-20 20:41:29 +0530

    dto

--- src/main/java/com/rish889/rishFeb2024/controller/UserController.java
+++ src/main/java/com/rish889/rishFeb2024/controller/UserController.java
@@ -1,30 +1,43 @@
 package com.rish889.rishFeb2024.controller;
 
+import com.rish889.rishFeb2024.config.ApiPaths;
 import com.rish889.rishFeb2024.dto.user.UserRequestDto;
 import com.rish889.rishFeb2024.dto.user.UserResponseDto;
 import com.rish889.rishFeb2024.mapper.UserMapper;
 import com.rish889.rishFeb2024.model.User;
 import com.rish889.rishFeb2024.repository.UserRepository;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping(path = "/demo")
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@Slf4j
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping(path = "/add")
-    public @ResponseBody ResponseEntity<UserResponseDto> addNewUser(@Valid @RequestBody UserRequestDto dto) {
-        final User user = userRepository.save(UserMapper.convertToEntity(dto));
-        return ResponseEntity.ok(UserMapper.convertDto(user));
+    @PostMapping(ApiPaths.USERS)
+    public @ResponseBody ResponseEntity<UserResponseDto> saveUser(@Valid @RequestBody UserRequestDto requestDto) {
+        log.info("saveUser(). requestDto : {}", requestDto);
+        final User user = userRepository.save(UserMapper.convertToEntity(requestDto));
+
+        final UserResponseDto responseDto = UserMapper.convertDto(user);
+        log.info("saveUser() successful. responseDto : {}", responseDto);
+        return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    @GetMapping(ApiPaths.USERS)
+    public @ResponseBody ResponseEntity<List<UserResponseDto>> fetchUsers() {
+        log.info("fetchUsers().");
+        final List<User> users = userRepository.findAll();
+
+        final List<UserResponseDto> responseDto = users.stream().map(user -> UserMapper.convertDto(user)).collect(Collectors.toList());
+        log.info("fetchUsers() successful. responseDto : {}", responseDto);
+        return ResponseEntity.ok(responseDto);
     }
 }
--- src/main/java/com/rish889/rishFeb2024/repository/UserRepository.java
+++ src/main/java/com/rish889/rishFeb2024/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.rish889.rishFeb2024.repository;
 
 import com.rish889.rishFeb2024.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
