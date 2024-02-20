94771741850ec1f3bcd74d676c73b9486962b3fa
Rishabh Garcha <rish889@gmail.com>
2024-02-20 18:46:19 +0530

    mysql

--- src/main/java/com/rish889/rishFeb2024/repository/UserRepository.java
+++ src/main/java/com/rish889/rishFeb2024/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.rish889.rishFeb2024.repository;public class UserRepository {
+package com.rish889.rishFeb2024.repository;
+
+import com.rish889.rishFeb2024.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
 }
