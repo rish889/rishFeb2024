e32e3844115bffccc673b47141cd801911cf0bc1
Rishabh Garcha <rish889@gmail.com>
2024-03-08 12:54:46 +0530

    Limited Time Deals

--- src/main/java/com/rish889/rishFeb2024/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rish889.rishFeb2024.repository;
-
-import com.rish889.rishFeb2024.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- src/main/java/com/rish889/rishFeb2024/service/DealService.java
+++ src/main/java/com/rish889/rishFeb2024/service/DealService.java
@@ -1,9 +1,6 @@
 package com.rish889.rishFeb2024.service;
 
 import com.rish889.rishFeb2024.model.Deal;
-import com.rish889.rishFeb2024.model.User;
-
-import java.util.List;
 
 public interface DealService {
     Deal createDeal(Deal deal);
